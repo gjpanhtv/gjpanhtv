a1f0a8a1321093185cf7ba38d711aaeba33fa7e8
gjpanhtv <gjpanhtv@gmail.com>
2024-09-15 07:00:14 +0000

    a

--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
-// js/index.js
-console.log("Hello from index.js!");
+document.addEventListener('DOMContentLoaded', () => {
+    const fileList = document.getElementById('fileList');
+    const repo = 'gjpanhtv/gjpanhtv.github.io'; // Thay đổi tên tài khoản và tên repo
+
+    fetch(`https://api.github.com/repos/${repo}/contents/app_tvbox`)
+        .then(response => response.json())
+        .then(files => {
+            files.forEach(file => {
+                if (file.type === 'file') {
+                    const listItem = document.createElement('li');
+                    listItem.innerHTML = `<a href="${file.download_url}" download>${file.name}</a>`;
+                    fileList.appendChild(listItem);
+                }
+            });
+        })
+        .catch(err => {
+            console.error('error:', err);
+        });
+});
